4099b0fedec638f56a4ef4b9056933e6956c19af
MRNSJ26 <Jijjinoor@gmail.com>
2023-09-24 09:22:38 +0300

    Second Commit - fixing the inputStream in the client and server

--- Client.java
+++ Client.java
@@ -6,32 +6,36 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) throws Exception {
         
-            Socket s = new Socket("localhost", 7070);
-            OutputStream outputStream = s.getOutputStream();
-            PrintWriter writer = new PrintWriter(outputStream);
-
+        try (
+            //connecting to the server
+            Socket s = new Socket("127.0.0.1", 7070);
+        ){        
             System.out.print("Enter a message for the server: ");
+
+            //Registering a message from the user
             Scanner scanner = new Scanner(System.in);
             String msgFromClient = scanner.nextLine();
-            scanner.next();
+            scanner.close();
             System.out.println("Client's Message: " + msgFromClient);
-
             System.out.println("sending a message to the Server...");
-            writer.write(msgFromClient);
+
+            //Sending the message to the server
+            OutputStream outputStream = s.getOutputStream();
+            PrintWriter writer = new PrintWriter(outputStream);
+            writer.println(msgFromClient);
             writer.flush();
             System.out.println("Message is sent");
 
+            //Receinvg the replay from the server
             System.out.println("Waiting for replay");
-            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(s.getInputStream())); 
-            String messageFromServer = inFromServer.readLine();
-
-
-            System.out.println(messageFromServer);
+            InputStream input = s.getInputStream();
+            InputStreamReader reader = new InputStreamReader(input);
+            BufferedReader bufReader = new BufferedReader(reader);
+            System.out.println(bufReader.readLine());
             System.out.println("Bye for now");
 
-        // InputStream input = s.getInputStream();
-        // InputStreamReader reader = new InputStreamReader(input);
-        // StringBuilder data = new StringBuilder();
-
+        } catch (IOException e) {
+            System.err.println(e);
+        }
     }
 }
--- Server.java
+++ Server.java
@@ -1,53 +1,46 @@
 package Labs.Lab1;
+
 import java.io.*;
 import java.net.*;
 
-
 public class Server {
     public static void main(String[] args) throws Exception{
+        try(
+            //Opeing port on the machine. 
             ServerSocket serverSocket = new ServerSocket(7070);
-    
-
+        ) {
             System.out.println("Server is running and waiting for clients...");
             
+            //waiting for the client to connect
             Socket clientSocket = serverSocket.accept();
             System.out.println("Client connected.");
 
-
-            OutputStream outputStream = clientSocket.getOutputStream();
-            PrintWriter writer = new PrintWriter(outputStream);
-
+            //Recieving the client's input
             InputStream input = clientSocket.getInputStream();
             InputStreamReader reader = new InputStreamReader(input);
             BufferedReader inFromClient = new BufferedReader(reader);     
             String messageFromClinet = inFromClient.readLine();
-
             System.out.println("Clients Message: " + messageFromClinet);
+
+            //sending a replay to the client
+            OutputStream outputStream = clientSocket.getOutputStream();
+            PrintWriter writer = new PrintWriter(outputStream);
 
             if(messageFromClinet.equals("ping")){
                 System.out.println("Sending a message for the client...");
-                writer.write("pong\n");
+                writer.println("pong\n");
                 writer.flush();
                 clientSocket.close();
-            }        
-            System.out.println("Message sent :), Bye for Now");
-       
-         // InputStream input = clientSocket.getInputStream(); 
-        // InputStreamReader reader = new InputStreamReader(input); 
-        // int character; 
-        // StringBuilder data = new StringBuilder(); 
-        // while((character = reader.read()) != -1) {
-        //     data.append((char) character); 
-        // }
-
-
-        // OutputStream output = s.getOutputStream();
-        // OutputStreamWriter writer = new OutputStreamWriter(output);
-        // writer.write("Hello from server");
-        // writer.flush();
-
-        // InputStream input  = s.getInputStream();
-        // InputStreamReader reader = new InputStreamReader(input);
-        // System.out.println(reader.read());
-    }}
-    
+            } else {
+                System.out.println("Sending a message for the client...");
+                writer.println("Loser =)\n");
+                writer.flush();
+                clientSocket.close();
+            }
+            System.out.println("Message sent :), Bye for Now"); 
+            
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+}
